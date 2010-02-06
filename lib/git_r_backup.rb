a077e3d513d37cf49340f9b9c4c048b6fdab3365
Slippy Douglas <git@6bitt.com>
2010-02-06 09:00:17 -0800

    Fixed a var name typo.

--- lib/git_r_backup.rb
+++ lib/git_r_backup.rb
@@ -11,13 +11,13 @@ class GitRBackup
     @app_dir = File.join(options[:base], options[:app]) if options[:app]
     @app_assets_dir = File.join(options[:base], options[:assets]) if options[:assets]
     
-    @ap_db_dump_path = File.join(@app_dir, 'db', 'data.yml') if @app_dir
+    @app_db_dump_path = File.join(@app_dir, 'db', 'data.yml') if @app_dir
     
     puts "backup cache repo dir:\t#{@backup_cache_repo_dir.inspect}"
     puts "backup cache sub dir:\t#{@backup_cache_sub_dir.inspect}" if @backup_cache_sub_dir
     puts "app dir:\t#{@app_dir.inspect}" if @app_dir
     puts "app assets dir:\t#{@app_assets_dir.inspect}" if @app_assets_dir
-    puts "app db dump path:\t#{@ap_db_dump_path.inspect}" if @ap_db_dump_path
+    puts "app db dump path:\t#{@app_db_dump_path.inspect}" if @app_db_dump_path
   end
   
   
