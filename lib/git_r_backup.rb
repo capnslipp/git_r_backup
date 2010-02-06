ecf407cbcc57cd739c17324ffca5acb94d803fcf
Slippy Douglas <git@6bitt.com>
2010-02-06 09:25:01 -0800

    A bit of clean-up; fixed a couple names.

--- lib/git_r_backup.rb
+++ lib/git_r_backup.rb
@@ -3,18 +3,21 @@ class GitRBackup
   
   
   def initialize(options)
+    raise ":base option is required" unless options[:base] && !options[:base].empty?
+    raise ":cache option is required" unless options[:cache] && !options[:cache].empty?
+    
     @backup_cache_repo_dir = File.join(options[:base], options[:cache])
-    @backup_cache_sub_dir = File.join(@cache_path, options[:sub])
-    @app_dir = File.join(options[:base], options[:app]) if options[:app]
-    @app_assets_dir = File.join(options[:base], options[:assets]) if options[:assets]
-    
-    @app_db_dump_path = File.join(@app_dir, 'db', 'data.yml') if @app_dir
-    
+    @backup_cache_sub_dir = File.join(@backup_cache_repo_dir, options[:sub]) if options[:sub]
     puts "backup cache repo dir:\t#{@backup_cache_repo_dir.inspect}"
     puts "backup cache sub dir:\t#{@backup_cache_sub_dir.inspect}" if @backup_cache_sub_dir
+    
+    @app_dir = File.join(options[:base], options[:app]) if options[:app]
+    @app_db_dump_path = File.join(@app_dir, 'db', 'data.yml') if @app_dir
     puts "app dir:\t#{@app_dir.inspect}" if @app_dir
-    puts "app assets dir:\t#{@app_assets_dir.inspect}" if @app_assets_dir
     puts "app db dump path:\t#{@app_db_dump_path.inspect}" if @app_db_dump_path
+    
+    @assets_dir = File.join(options[:base], options[:assets]) if options[:assets]
+    puts "assets dir:\t#{@assets_dir.inspect}" if @assets_dir
   end
   
   
