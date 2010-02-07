05b17a569c5a777344a79f33fe74802dbe328ebc
Slippy Douglas <git@6bitt.com>
2010-02-07 15:29:26 -0800

    Fixed some stuff. • added support for absolute :cache and :app paths • made sure "rake db:dump" is run in the app directory • added raises when args are needed, but not present • removed usages of "/" for paths when not needed

--- lib/git_r_backup.rb
+++ lib/git_r_backup.rb
@@ -10,26 +10,32 @@ class GitRBackup
     raise ":base option is required" unless options[:base] && !options[:base].empty?
     raise ":cache option is required" unless options[:cache] && !options[:cache].empty?
     
-    @backup_cache_repo_dir = File.join(options[:base], options[:cache])
+    @backup_cache_repo_dir = (options[:cache][0, 1] == File::SEPARATOR) ? options[:cache] : File.join(options[:base], options[:cache])
     @backup_cache_sub = options[:sub]
     @backup_cache_sub_dir = File.join(@backup_cache_repo_dir, @backup_cache_sub) if @backup_cache_sub
     
-    @app_dir = File.join(options[:base], options[:app]) if options[:app]
-    @app_db_data_dump_path = File.join(@app_dir, 'db', 'data.yml') if @app_dir
-    @app_db_schema_dump_path = File.join(@app_dir, 'db', 'schema.rb') if @app_dir
+    if options[:app]
+      @app_dir = (options[:app][0, 1] == File::SEPARATOR) ? options[:app] : File.join(options[:base], options[:app])
+      @app_db_data_dump_path = File.join(@app_dir, 'db', 'data.yml')
+      @app_db_schema_dump_path = File.join(@app_dir, 'db', 'schema.rb')
+    end
     
     @assets_dir = File.join(options[:base], options[:assets]) if options[:assets]
   end
   
   
   def backup_db
-    system 'rake db:dump'
+    raise ":app option is required" unless @app_dir && !@app_dir.empty?
+    
+    Dir.chdir(@app_dir) do
+      system 'rake db:dump'
+    end
     
     full_app_db_data_dump_path = File.expand_path(@app_db_data_dump_path)
     full_app_schema_data_dump_path = File.expand_path(@app_db_schema_dump_path)
     in_dest_cache_dir do
-      FileUtils.cp full_app_db_data_dump_path, './'
-      FileUtils.cp full_app_schema_data_dump_path, './'
+      FileUtils.cp full_app_db_data_dump_path, '.'
+      FileUtils.cp full_app_schema_data_dump_path, '.'
       
       system 'git add -A' # there seems to be no option to do this with Git::Base
     end
@@ -46,9 +52,11 @@ class GitRBackup
   
   
   def backup_assets
+    raise ":assets option is required" unless @assets_dir && !@assets_dir.empty?
+    
     full_assets_dir = File.expand_path(@assets_dir)
     in_dest_cache_dir do
-      FileUtils.cp_r full_assets_dir, './'
+      FileUtils.cp_r full_assets_dir, '.'
       
       system 'git add -A' # there seems to be no option to do this with Git::Base
     end
